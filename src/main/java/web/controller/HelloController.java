61e9080bb30dbefe94f26074f7a9c1d5330408f0
anton <antonsv616@gmail.com>
2024-05-07 11:55:17 +0300

    Task5

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -1,15 +1,15 @@
 package web.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class HelloController {
+
 
 	@GetMapping(value = "/")
 	public String printWelcome(ModelMap model) {
@@ -20,5 +20,4 @@ public class HelloController {
 		model.addAttribute("messages", messages);
 		return "index";
 	}
-	
 }
